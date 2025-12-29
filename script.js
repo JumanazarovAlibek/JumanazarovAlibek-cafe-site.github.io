9db728723db79ff9718d3573816f5a9be7dd0f0a
JumanazarovAlibek <alibekjumanazarov2011@gmail.com>
2025-12-29 14:48:43 +0500

    to'liq versiya

--- script.js
+++ script.js
@@ -5,4 +5,10 @@ burger.addEventListener("click", () => {
   navLinks.classList.toggle("active");
 });
 
+const links = navLinks.querySelectorAll("a");
+links.forEach(link => {
+  link.addEventListener("click", () => {
+    navLinks.classList.remove("active");
+  });
 
+});
